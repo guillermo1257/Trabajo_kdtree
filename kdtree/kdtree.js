5bb97dba8e79e60c4e906e08e5e0aa2d74420b90
edus124 <edus124@gmail.com>
2022-09-04 00:06:36 -0500

    practica 4: hasta ejercicio 8

--- kdtree/kdtree.js
+++ kdtree/kdtree.js
@@ -28,11 +28,17 @@ function getHeight ( node ) {
 
 function generate_dot ( node ) 
 {
+    g =  dot_nodes(node);
+    return 'digraph G {' + g + '}';
+}
+
+function dot_nodes ( node ) 
+{
     g = '';
     if(node !== null)
     {
-        g += this.generate_dot(node.left);
-        g += this.generate_dot(node.right);
+        g += this.dot_nodes(node.left);
+        g += this.dot_nodes(node.right);
        
         if (node.left)
             g +=  '"' + node.point  + '"'+ ' -> ' + '"' + node.left.point + '";'
@@ -42,24 +48,27 @@ function generate_dot ( node )
     }
     else
     {
-        //f = 'digraph G {' + g + '}';
-        //console.log(f); 
         return '';
     }
 }
+
 
 function build_kdtree(points, depth = 0){
     var n = points.length;
     var axis = depth % k;
     
-    
     if (n <= 0){
         return null;
-        }
-            if (n == 1){
-            return new Node(points[0], axis)
     }
-    
+
+    if (n == 1)
+    {
+        //stroke (Math.floor(Math.random() * 255) + 100 , Math.floor(Math.random() * 255) + 100  , Math.floor(Math.random() * 255) + 100) ;
+        //line(points[0][0],0,points[0][0],width);
+
+        return new Node(points[0], axis)
+    }
+
     var median = Math.floor(points.length / 2);
     
     // sort by the axis
@@ -74,10 +83,13 @@ function build_kdtree(points, depth = 0){
     
     //console.log(right);
     
+    stroke (Math.floor(Math.random() * 255) + 100 , Math.floor(Math.random() * 255) + 100  , Math.floor(Math.random() * 255) + 100) ;
+
     var node = new Node(points[median].slice(0, k), axis);
-    node.left = build_kdtree(left, depth + 1);
+    node.left = build_kdtree(left, depth + 1); 
+
     node.right = build_kdtree(right, depth + 1);
-    
+
     return node;
 }
 
@@ -91,15 +103,81 @@ function distanceSquared ( point1 , point2 ){
 function closest_point_brute_force ( points , point) 
 {
     dist_menor = 999999999999999;
+    point_menor = [];
     for (var i = 0; i < points.length; i ++)
     {
         point_t = points[i];
         dist = distanceSquared (point_t, point);
+        console.log(point_t + '|||' + dist);
         if (dist < dist_menor)
         {
             dist_menor = dist;
+            point_menor = point_t;
         }
     }
-    return dist_menor;
+    return point_menor;
 }
-function naive_closest_point (node , point , depth = 0, best = null ) {}
+
+function naive_closest_point (node , point , depth = 0, best = null ) 
+{
+	if (!node) 
+        return best; 
+
+    if(best==null || distanceSquared(best.point,point)>distanceSquared(node.point,point)){
+        best = node;
+    }
+
+	if (node.point[node.axis] > point[node.axis])
+	{
+		if(node.left) 
+            best = naive_closest_point(node.left,point,depth++,best);
+	} 
+	else
+	{
+		if(node.right) 
+            best = naive_closest_point(node.right,point,depth++,best);
+	}
+	return best;
+}
+
+
+function closer_point(point,p1,p2)
+{
+	if(!p1) 
+        return p2;
+	if(!p2) 
+        return p1;
+
+	if(distanceSquared(point,p1.point) > distanceSquared(point,p2.point)) 
+        return p2;
+    else
+	    return p1;
+}
+
+function closest_point(node,point_2,depth = 0)
+{
+	if (node == null) 
+        return null; 
+	var next_branch  = null;
+	var opposite_branch  = null;
+	if (node.point[node.axis] > point_2[node.axis])
+	{
+		next_branch  = node.left;
+		opposite_branch  = node.right;
+	} 
+	else
+	{
+		next_branch  = node.right;
+		opposite_branch  = node.left;
+	}
+	
+	var best = closer_point(point_2,closer_point(point_2,closest_point(next_branch ,point_2,depth+1),node),best);
+	
+	if (distanceSquared(best.point,point_2) > Math.abs(point_2[node.axis] - node.point[node.axis]))
+	{
+		best2 = closer_point(point_2,closest_point(opposite_branch ,point_2,depth+1),node);
+	}
+	best = closer_point(point_2,best2,best);
+	
+	return best;	
+}
--- kdtree/sketch.js
+++ kdtree/sketch.js
@@ -5,52 +5,67 @@ function setup () {
   background (0) ;
   for (var x = 0; x < width; x += width / 10) {
     for (var y = 0; y < height; y += height / 5) {
-      stroke (125 , 125 , 125) ;
+      stroke (50 , 50 , 50) ;
       strokeWeight (1) ;
       line (x, 0, x, height );
       line (0 , y, width , y);
       }
   }
       
-  var data = [];
-  for ( let i = 0; i < 10; i ++) {
-    var x = Math.floor ( Math.random () * height );
-    var y = Math.floor ( Math.random () * height );
-    data.push ([x, y]) ;
+  // var data = [];
+  // for ( let i = 0; i < 10; i ++) {
+  //   var x = Math.floor ( Math.random () * height );
+  //   var y = Math.floor ( Math.random () * height );
+  //   data.push ([x, y]) ;
     
-    fill (255 , 255 , 255) ;
-    circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
-    textSize (8) ;
-    text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
+  //   fill (255 , 255 , 255) ;
+  //   circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
+  //   textSize (8) ;
+  //   text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
+  // }
+var data = [
+  // [40,70],
+  // [70,130],
+  // [90,40],
+  // [110,100],
+  // [140,110],
+  // [160,100],
+  // [150,30]
+  // [40 ,70] ,
+  [40 ,70] ,
+  [70 ,130] ,
+  [90 ,40] ,
+  [110 , 100] ,
+  [140 ,110] ,
+  [160 , 100],
+  [150,30]
+
+  ];
+
+  for ( let i = 0; i < data.length; i ++) {
+    
+       var x = data[i][0];
+       var y = data[i][1];
+       fill (255 , 255 , 255) ;
+       circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
+       textSize (8) ;
+       text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
   }
 
-  
-  // var data = [
-  //   [40 ,70] ,
-  //   [70 ,130] ,
-  //   [90 ,40] ,
-  //   [110 , 100] ,
-  //   [140 ,110] ,
-  //   [160 , 100]
-  //   ];
-
-  // for ( let i = 0; i < data.length; i ++) {
-    
-  //     var x = data[i,0];
-  //     var y = data[i,1];
-  //     fill (255 , 255 , 255) ;
-  //     circle (x, y, 7) ; // 200 -y para q se dibuje apropiadamente
-  //     textSize (8) ;
-  //     text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
-  //   }
   console.log("MENOR");
-  console.log(closest_point_brute_force(data, [100, 100]));
+  console.log(closest_point_brute_force(data, [140, 90]));
 
   var root = build_kdtree ( data ) ;
-  console.log(getHeight(root));
-  console.log ( root.point );
+  //console.log(getHeight(root));
+  //console.log ( root.point );
   
-  //console.log('digraph G {' + generate_dot(root) + '}');
+  console.log( generate_dot(root));
+
+  best1 = naive_closest_point(root, [140, 90]);
+  
+  console.log("naive_closest_point " + best1.point);
  
-  
+  best2 = closest_point(root, [140, 90]);
+  console.log("closest_point " + best2.point);
+  console.log();
 }
